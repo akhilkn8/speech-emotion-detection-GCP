02745d12848875146b4e7cf9a2b6a193ce76f7a8
Debanjan Saha <debanjansh@gmail.com>
2024-04-19 21:47:23 -0400

    fixed bash scripts

--- gcr_data_gen.sh
+++ gcr_data_gen.sh
@@ -2,6 +2,6 @@ cd data_generation
 
 docker buildx build -t data_gen_img --platform linux/amd64 .
 
-docker tag data_gen_img us-east4-docker.pkg.dev/firm-site-417617/data-generation/data_gen_img:staging
+docker tag data_gen_img us-east1-docker.pkg.dev/firm-site-417617/data-generation/data_gen_img:staging
 
-docker push us-east4-docker.pkg.dev/firm-site-417617/data-generation/data_gen_img:staging
+docker push us-east1-docker.pkg.dev/firm-site-417617/data-generation/data_gen_img:staging
--- gcr_model_train.sh
+++ gcr_model_train.sh
@@ -2,6 +2,6 @@ cd model_training
 
 docker buildx build -t model_train_img --platform linux/amd64 .
 
-docker tag model_train_img us-east4-docker.pkg.dev/firm-site-417617/model-training/model_train_img:staging
+docker tag model_train_img us-east1-docker.pkg.dev/firm-site-417617/model-training/model_train_img:staging
 
-docker push us-east4-docker.pkg.dev/firm-site-417617/model-training/model_train_img:staging
+docker push us-east1-docker.pkg.dev/firm-site-417617/model-training/model_train_img:staging
