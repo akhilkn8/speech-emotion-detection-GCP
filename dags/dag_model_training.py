2eb3b011fdf634ab918a1fbf1608839429bccd0e
Akhil Krishna <akhilkn8@gmail.com>
2024-04-20 23:07:08 -0400

    model training DAG  for cloud composer

--- dags/dag_model_training.py
+++ dags/dag_model_training.py
@@ -1,6 +1,12 @@
 from datetime import datetime, timedelta
+from typing import Optional
+import google.cloud.aiplatform as aiplatform
 from airflow import DAG
 from airflow.providers.google.cloud.operators.cloud_run import CloudRunExecuteJobOperator
+from airflow.providers.google.cloud.operators.vertex_ai.custom_job import (
+    CreateCustomContainerTrainingJobOperator,
+    DeleteCustomTrainingJobOperator,
+)
 
 
 default_args = {
@@ -12,17 +18,19 @@ default_args = {
 }
 
 with DAG(
-    "data_generation_dag",
+    "model_training_dag",
     default_args=default_args,
     schedule_interval=None,
     catchup=False,
-) as dag_generation:
+) as dag_training:
 
-    execute1 = CloudRunExecuteJobOperator(
-        task_id='Data-Generation',
+    create_custom_container_training_job = CreateCustomContainerTrainingJobOperator(
+        task_id="speech-model-training",
+        staging_bucket=f"gs://artifacts-speech-emotion/model_training",
+        display_name='model_train_img',
+        container_uri='us-east1-docker.pkg.dev/firm-site-417617/model-training/model_train_img@sha256:e01127c1877665887164ca656dc25c11dcddf1ddb2f3026e1b523d01b20fa224',
+        # model_serving_container_image_uri=MODEL_SERVING_CONTAINER_URI,
+        # run params
+        region="us-east1",
         project_id='firm-site-417617',
-        region='us-east4',
-        job_name='data-gen-img',
-        dag=dag_generation,
-        deferrable=False,
     )
